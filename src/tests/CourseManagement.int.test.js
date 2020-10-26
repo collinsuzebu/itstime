9a251cf677f64875901a644dd352961b9587e848
Collins Uzebu <uzebuc@gmail.com>
2020-10-26 11:25:31 +0000

    included api and proper config

---
+++ src/tests/CourseManagement.int.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { mount } from "enzyme";
+import { CourseManagement } from "../components/courses/CourseManagement";
+import { subjects, courses, newCourse } from "./testDataMock";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function renderCourseManagement(args) {
+  const defaultProps = {
+    subjects,
+    courses,
+    history: {},
+    saveCourse: jest.fn(),
+    loadCourses: jest.fn(),
+    loadSubjects: jest.fn(),
+    course: newCourse,
+    match: {},
+  };
+
+  const props = { ...defaultProps, ...args };
+  return mount(
+    <Router>
+      <CourseManagement {...props} />
+    </Router>
+  );
+}
+
+it("sets error when attempting to submit an empty title field", () => {
+  const wrapper = renderCourseManagement();
+  wrapper.find("form").simulate("submit");
+  const error = wrapper.find(".alert").first();
+  expect(error.text()).toBe("Subject is required");
+});
