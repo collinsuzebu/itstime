625e66eafec8cd14ab7b7bcf35f1320a98c35be4
Collins Uzebu <uzebuc@gmail.com>
2020-10-26 06:59:00 +0000

    removed build dir from git ignore

---
+++ build/precache-manifest.612c75e0752a3b58caefad4d8c1ea56b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3876cc73da6176b16daff4fef357996a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43dc1d68b67a598b4296",
+    "url": "/static/css/main.2927756a.chunk.css"
+  },
+  {
+    "revision": "e9a6825112096c580588",
+    "url": "/static/js/2.192d41e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.192d41e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dc1d68b67a598b4296",
+    "url": "/static/js/main.965eb1c9.chunk.js"
+  },
+  {
+    "revision": "62c023afb08b2af9d439",
+    "url": "/static/js/runtime-main.e2edf3ea.js"
+  },
+  {
+    "revision": "1e9539d819557945a38880c47391a7f1",
+    "url": "/static/media/error404.1e9539d8.png"
+  },
+  {
+    "revision": "f82f584a9ce069713634e79c1244aeda",
+    "url": "/static/media/under-construction.f82f584a.jpg"
+  }
+]);
