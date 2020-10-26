70111f203302968c88a5517ef231b55c63776b16
Collins Uzebu <uzebuc@gmail.com>
2020-10-26 01:32:41 +0000

    configured enzymes and jest

---
+++ src/setupTests.js
@@ -0,0 +1,7 @@
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+// import fetchMock from "jest-fetch-mock";
+
+configure({ adapter: new Adapter() });
+
+// fetchMock.enableMocks();
