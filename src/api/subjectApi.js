191f7f8c2edaf48c9f4cb544a17678673b27492c
Collins Uzebu <uzebuc@gmail.com>
2020-11-04 11:39:38 +0000

    removed subject api files

--- src/api/subjectApi.js
+++
@@ -1,6 +0,0 @@
-import { handleResponse, handleError } from "./apiUtils";
-const baseUrl = process.env.REACT_APP_API_URL + "/subjects/";
-
-export function getSubjects() {
-  return fetch(baseUrl).then(handleResponse).catch(handleError);
-}
