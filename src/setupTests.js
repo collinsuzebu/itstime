c0d7a26a0604d1083fcb46fb0daa9f202b4d15d6
Collins Uzebu <uzebuc@gmail.com>
2020-11-04 11:38:46 +0000

    http request mock service

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,13 @@
+import "mutationobserver-shim";
+import "@testing-library/jest-dom/extend-expect";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-// import fetchMock from "jest-fetch-mock";
 
+import "../src/api/testServer";
+
+global.MutationObserver = window.MutationObserver;
 configure({ adapter: new Adapter() });
 
+// import "@testing-library/jest-dom";
+// import fetchMock from "jest-fetch-mock";
 // fetchMock.enableMocks();
